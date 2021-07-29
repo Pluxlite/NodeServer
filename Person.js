b0a0838e12e3b066673859571f12bd56fec6a89f
Robert <rbowensoftware@gmail.com>
2021-07-28 22:06:33 -0400

    Testing out Moduel Wrapper Function

--- Person.js
+++ Person.js
@@ -1,3 +1,10 @@
+//module wrapper Function
+(function (exports, require, module, __filename, __dirname){
+
+})
+
+console.log(__dirname, __filename)
+
 class Person {
     constructor(name, age){
         this.name = name;
